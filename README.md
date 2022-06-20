1c066142e844e5d58b9f937fec3a84305a3b9d68
anthonymorton <anthonymorton760@gmail.com>
2022-06-20 14:26:24 -0700

    uploading the inital script for the respiratory protection database

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # RespiratoryFitTestDatabase
+
+## Info
+
+This is the master script for the Respiratory Fit Test Database. This was created using Microsoft SQL Server Management Studio. This will serve as the database for the respiratory fit test project. Changes to the database will be noted in the change log.
+
+## Change Log
+
+### Initial Creation/Upload - 6/20/2022
+
+Initial creation and upload of the Respiratory Fit Test Database.
---
+++ RespiratoryProtectionDatabase.sql
@@ -0,0 +1,115 @@
+USE [master]
+GO
+
+/****** Object:  Database [RespiratoryProtection]    Script Date: 6/20/2022 2:20:20 PM ******/
+CREATE DATABASE [RespiratoryProtection]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'RespiratoryProtection', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\RespiratoryProtection.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'RespiratoryProtection_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.SQLEXPRESS\MSSQL\DATA\RespiratoryProtection_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [RespiratoryProtection].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET ANSI_NULL_DEFAULT OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET ANSI_NULLS OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET ANSI_PADDING OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET ANSI_WARNINGS OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET ARITHABORT OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET AUTO_CLOSE OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET AUTO_SHRINK OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET AUTO_UPDATE_STATISTICS ON 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET CURSOR_DEFAULT  GLOBAL 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET NUMERIC_ROUNDABORT OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET QUOTED_IDENTIFIER OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET RECURSIVE_TRIGGERS OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET  DISABLE_BROKER 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET TRUSTWORTHY OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET PARAMETERIZATION SIMPLE 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET HONOR_BROKER_PRIORITY OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET RECOVERY SIMPLE 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET  MULTI_USER 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET PAGE_VERIFY CHECKSUM  
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET DB_CHAINING OFF 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET DELAYED_DURABILITY = DISABLED 
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET QUERY_STORE = OFF
+GO
+
+ALTER DATABASE [RespiratoryProtection] SET  READ_WRITE 
+GO
+
